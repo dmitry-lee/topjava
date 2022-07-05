20443d59e98775d304ef41813d702e33546cca07
DLL <dmitry.lee@yandex.ru>
2022-07-05 22:57:02 +0300

    HW5 fix after review 3

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -2,7 +2,6 @@ package ru.javawebinar.topjava.service;
 
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -15,7 +14,6 @@ import static ru.javawebinar.topjava.util.DateTimeUtil.atStartOfNextDayOrMax;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
-@Transactional(readOnly = true)
 public class MealService {
 
     private final MealRepository repository;
@@ -28,7 +26,6 @@ public class MealService {
         return checkNotFoundWithId(repository.get(id, userId), id);
     }
 
-    @Transactional
     public void delete(int id, int userId) {
         checkNotFoundWithId(repository.delete(id, userId), id);
     }
@@ -41,13 +38,11 @@ public class MealService {
         return repository.getAll(userId);
     }
 
-    @Transactional
     public void update(Meal meal, int userId) {
         Assert.notNull(meal, "meal must not be null");
         checkNotFoundWithId(repository.save(meal, userId), meal.id());
     }
 
-    @Transactional
     public Meal create(Meal meal, int userId) {
         Assert.notNull(meal, "meal must not be null");
         return repository.save(meal, userId);
